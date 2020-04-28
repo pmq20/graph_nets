b29ee9331cfb7ea74aa58e535900d7e31e4cb1dc
Minqi Pan <pmq2001@gmail.com>
2020-04-28 18:10:48 -0400

    todo: sonnet

---
+++ main.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import itertools
+import time
+
+from graph_nets import graphs
+from graph_nets import utils_np
+from graph_nets import utils_tf
+from graph_nets.demos import models
+
+print('.')
